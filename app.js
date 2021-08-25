6dae3e9200a1b8743cb76ed80175fae3d76c8445
Ben Gittins <bengittins@ben-mbp.local>
2021-08-25 15:11:57 -0700

    Reformat, prettier, start cmd

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
+const delayMs = 14000;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   console.log("Hello");
-  setTimeout(() => { res.send('Hello World!'); }, 3600);
-
-})
+  setTimeout(() => {
+    res.send(
+      `Hello World! Delayed by ${delayMs / 1000} seconds (${delayMs}ms)`
+    );
+  }, delayMs);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
