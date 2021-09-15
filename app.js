ee8c50c7e421e2f4ff916abeeee71aa0b11d1cef
Ben Gittins <bengittins@ben-mbp.local>
2021-09-15 09:22:48 -0700

    Add style

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const delayMs = 14000;
+const delayMs = 1000;
 
 app.get("/", (req, res) => {
   console.log("Hello");
   setTimeout(() => {
     res.send(
-      `Hello World! Delayed by ${delayMs / 1000} seconds (${delayMs}ms)`
+      `
+      <style>
+        h1 {
+          margin: 0;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          -ms-transform: translate(-50%, -50%);
+          transform: translate(-50%, -50%);
+        }
+      </style>
+      <h1>Hello World! Delayed by ${delayMs / 1000} seconds (${delayMs}ms)</h1>`
     );
   }, delayMs);
 });
